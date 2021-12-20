7f2356ef3f8a19b461349318893413c35c2aad87
DmitriyUtyugov <utyugovkob15@gmail.com>
2021-12-20 11:21:36 +0500

    Configured EF Core usage

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -8,16 +8,26 @@ using System.Collections.Generic;
 using System.Linq;
 using SportsStore.Models;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace SportsStore
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+                Configuration["Data:SportsStoreProducts:ConnectionString"]));
+            services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
